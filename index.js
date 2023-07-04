6a3c521450af13d1251cc98461060d4b98ee6550
José Piña <pinajoma0@gmail.com>
2023-07-04 04:48:03 +0000

    class completed

---
+++ index.js
@@ -0,0 +1,32 @@
+import { ethers } from "./ethers.js"
+import { contractAddress, abi } from "./constants.js"
+
+async function connect() {
+    console.log("Hola")
+    if(typeof window.ethereum != "undefined") {
+        await ethereum.request({ method: 'eth_requestAccounts' })
+        console.log("Si, me conecte")
+    }
+}
+
+async function mint() {
+    const ethAmount = document.getElementById("ethAmount").value
+    
+    if(typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const address = await signer.getAddress();
+        const contract = new ethers.Contract(contractAddress,abi,signer)
+        await contract.mint(address,ethAmount.toString())
+        
+    }
+}
+
+window.connect = connect
+window.mint = mint
+
+
+// Try/catch
+// Que pasa si no se conecta a metamask
+// Valor equivocado
+// not owner 
